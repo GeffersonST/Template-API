a20ecf5a3ffd23e50e3e0e5ec4d3ae12b4cacc61
GeffersonST <geffsevero@gmail.com>
2021-09-16 13:38:18 -0300

    fetch API

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import React, { Component } from "react";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      nutri: [],
+    };
+  }
+
+  componentDidMount() {
+    let url = "https://sujeitoprogramador.com/rn-api/?api=posts";
+    fetch(url)
+      .then((r) => r.json())
+      .then((json) => {
+        let state = this.state;
+        state.nutri = json;
+        this.setState(state);
+        // console.log(json);
+      });
   }
   render() {
     return <div></div>;
